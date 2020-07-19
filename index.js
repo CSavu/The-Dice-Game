2cdd22899d7c325c06b8f8396de2dc968db0ddd2
CSavu <66332005+CSavu@users.noreply.github.com>
2020-07-19 09:46:16 +0300

    finished game

--- index.js
+++ index.js
@@ -15,9 +15,15 @@ while (dice1 === dice2) dice2 = makeDice(dice2);
 var string1 = "images/dice" + dice1.toString() + ".png";
 var string2 = "images/dice" + dice2.toString() + ".png";
 
-if (dice1 > dice2) var win = "Player 1";
-else var win = "Player 2";
-
+if (dice1 > dice2)
+{
+  document.querySelector(".trophy2").style.visibility = "hidden";
+  var win = "Player 1";
+} else
+{
+  document.querySelector(".trophy1").style.visibility = "hidden";
+  var win = "Player 2";
+}
 document.querySelector("#winner").textContent = win + " wins!";
 
 document.querySelector("#img1").setAttribute("src", string1);
