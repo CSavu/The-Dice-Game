bf856aa54c4127a461bdf8810f2f104ce8d0b52c
CSavu <66332005+CSavu@users.noreply.github.com>
2020-07-19 09:52:58 +0300

    added draw

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ function makeDice(dice)
 var dice1 = makeDice(dice1);
 var dice2 = makeDice(dice2);
 
-while (dice1 === dice2) dice2 = makeDice(dice2);
-
 var string1 = "images/dice" + dice1.toString() + ".png";
 var string2 = "images/dice" + dice2.toString() + ".png";
 
@@ -19,12 +17,18 @@ if (dice1 > dice2)
 {
   document.querySelector(".trophy2").style.visibility = "hidden";
   var win = "Player 1";
-} else
+} else if (dice2 > dice1)
 {
   document.querySelector(".trophy1").style.visibility = "hidden";
   var win = "Player 2";
+} else
+{
+  document.querySelector(".trophy2").style.visibility = "hidden";
+  document.querySelector(".trophy1").style.visibility = "hidden";
+  document.querySelector("#winner").textContent = "DRAW!";
 }
-document.querySelector("#winner").textContent = win + " wins!";
+
+if (dice1 !== dice2) document.querySelector("#winner").textContent = win + " wins!";
 
 document.querySelector("#img1").setAttribute("src", string1);
 document.querySelector("#img2").setAttribute("src", string2);
